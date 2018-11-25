b06a3b8a3cad60b79a1b4286c131a12d84d1c39d
Jamie Connolly <jamie.connolly@tailify.com>
2018-11-25 21:32:37 +0000

    Add in a basic Jest test

---
+++ tests/index.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const eslint = require('eslint');
+
+it('should not contain invalid rules', () => {
+  const cli = new eslint.CLIEngine({
+    configFile: './lib/index.js',
+    useEslintrc: false,
+  });
+
+  const report = cli.executeOnText(
+    'module.exports = {};\n'
+  );
+
+  expect(report).toBeTruthy();
+  expect(report.errorCount).toBe(0);
+  expect(report.warningCount).toBe(0);
+});
