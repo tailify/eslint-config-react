5277fde9649952a0db0dd22f4ff75f6bf6ade027
Jamie Connolly <jamie.connolly@tailify.com>
2019-08-15 10:26:52 +0100

    Remove the weak `toBeTruthy()` test

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,8 +9,6 @@ test('should not contain invalid rules', () => {
   });
 
   const report = cli.executeOnText('module.exports = {};\n');
-
-  expect(report).toBeTruthy();
   expect(report.errorCount).toBe(0);
   expect(report.warningCount).toBe(0);
 });
